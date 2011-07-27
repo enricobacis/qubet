41f851d25bd22d739aee3ce49f58a6f0eb576147
ciriello.daniele@gmail.com <ciriello.daniele@gmail.com>
2011-07-27 10:30:46 +0200

    completato lo scheletro di alcuni file .cpp

--- obstacle.cpp
+++ obstacle.cpp
@@ -1,1 +1,47 @@
 #include "obstacle.h"
+
+Obstacle::Obstacle(GLint id, QObject *parent)
+{
+
+}
+
+Obstacle::~Obstacle()
+{
+
+}
+
+GLint Obstacle::getId()
+{
+
+}
+
+void Obstacle::setPosition(Vector3f *_position)
+{
+
+}
+
+Vector3f *Obstacle::getPosition()
+{
+
+}
+
+
+void Obstacle::setColor(QColor _color)
+{
+
+}
+
+QColor Obstacle::getColor()
+{
+
+}
+
+void Obstacle::setModelId(GLint _modelId)
+{
+
+}
+
+GLint Obstacle::getModelId()
+{
+
+}
--- positioncontroller.cpp
+++ positioncontroller.cpp
@@ -1,1 +1,26 @@
 #include "positioncontroller.h"
+
+PositionController::PositionController(Cube *_cube, Level *_level, QMap<Vector3f, GLint> *_obstacleModelsList, QObject *parent)
+{
+
+}
+
+PositionController::~PositionController()
+{
+
+}
+
+void PositionController::start()
+{
+
+}
+
+void PositionController::checkPosition()
+{
+
+}
+
+void PositionController::run()
+{
+
+}
