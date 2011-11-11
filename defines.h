816ade9c6eb8b02e5ed9e036638893d0da37bb72
enrico.bacis@gmail.com <enrico.bacis@gmail.com>
2011-11-11 21:18:35 +0100

    Aggiunta la define GL_CLAMP_TO_EDGES se non già definita. (Su windows manca)

--- defines.h
+++ defines.h
@@ -42,4 +42,7 @@ enum { LOADING, MENU_VIEW, GAME_VIEW, LEVELEDITOR_VIEW, NO_VIEW };
  */
 enum { STORY_MODE, ARCADE_MODE, EDITOR_MODE };
 
+// Re-define for compatibility purposes
+#define GL_CLAMP_TO_EDGE        0x812F
+
 #endif // DEFINES_H
